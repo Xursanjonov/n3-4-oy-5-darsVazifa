25497cf4c3be4d7866f8cb476adfba3aa9d88042
Nusratillo <nxursanjonov888@gmail.com>
2023-12-25 13:53:37 +0500

    Vazifa file two add

--- Vazifa/JS/accardion.js
+++ Vazifa/JS/accardion.js
@@ -1,1 +1,13 @@
-let 
+const accardion__title = document.querySelectorAll(".accardion__title");
+
+const openAccardion = (e) => {
+  for(let i of accardion__title) {
+    i.parentNode.classList.remove("active");
+  }
+  e.target.parentNode.classList.add("active");
+  e.target.style.color = "red";
+};
+
+for(let a of accardion__title) {
+  a.addEventListener("click", openAccardion);
+}
